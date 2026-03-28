cc7534d839b8af26113e1c7dff31c1aace740fa7
Ross Devine <roscod@live.ie>
2026-03-28 10:37:06 +0000

    Change max quantity to 500ml (avoid brew basket spillover)

--- overbrew/src/constants/brew.ts
+++ overbrew/src/constants/brew.ts
@@ -3,4 +3,4 @@ export const MAX_TEMP = 98
 export const MIN_FLOW_RATE = 5
 export const MAX_FLOW_RATE = 25
 export const MIN_QUANTITY = 250
-export const MAX_QUANTITY = 1000
+export const MAX_QUANTITY = 500
